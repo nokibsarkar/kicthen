7f85c098f35624d979ea0d8e81a8292657c05e56
nokibsarkar <nokibsarkar@gmail.com>
2024-12-21 20:28:45 +0600

    added test case

---
+++ test.py
@@ -0,0 +1,34 @@
+from fastapi.testclient import TestClient
+from src import app
+from src.database import engine
+from src.database.ingredients import Ingredient
+from src.database import Session
+import pytest
+import json
+
+client = TestClient(app)
+
+@pytest.fixture
+def db():
+    with Session(engine) as session:
+        yield session
+
+
+def test_create_ingredient(db):
+    response = client.post(
+        "/ingredients/",
+        json={"name": "Sugar", "quantity": 5, "unit": "kg"}
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"]["name"] == "Sugar"
+    assert data["data"]["quantity"] == 5
+    assert data["data"]["unit"] == "kg"
+    assert data["success"] == True
+
+def test_get_all_ingredients(db):
+    response = client.get("/ingredients/")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data["data"]) > 0
+    assert data["success"] == True
